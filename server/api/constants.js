25d656c423318075a811f92cd3bd5acc7e1b37a2
prikshit <prikshittekta@gmail.com>
2017-08-22 18:43:32 +0530

    image validation on server

---
+++ server/api/constants.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MAXIMUM_UPLOAD_FILE_SIZE: 10000000,     //IN BYTES - 10MB
+    ACCEPTED_IMAGE_TYPES: ['image/jpeg', 'image/png']
+}
--- server/api/uploads/multer.js
+++ server/api/uploads/multer.js
@@ -1,6 +1,7 @@
 const multer = require('multer');
 const Promise = require('bluebird');
 const responseFlags = require('../responseFlags');
+const constants = require('../constants');
 
 let storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -11,13 +12,32 @@ let storage = multer.diskStorage({
     }
 })
 
-let uploadMulter = multer({ storage: storage });
+let uploadMulter = multer({
+    storage: storage,
+    limits: {
+        fileSize: constants.MAXIMUM_UPLOAD_FILE_SIZE
+    },
+    fileFilter: (req, file, callback) => {
+        if (constants.ACCEPTED_IMAGE_TYPES.indexOf(file.mimetype) < 0) {
+            let err = new Error('Image type not acceptable.')
+            return callback(err, false)
+        }
+        callback(null, true);
+    }
+});
 
+/**
+ * Function to set the multer field name and the number of images being uploaded
+ * 
+ * @param {String} fieldname 
+ * @param {Integer} imageCount 
+ * @returns a function
+ */
 let uploadMultipart = (fieldname, imageCount) => {
-    let upload = uploadMulter.array(fieldname, imageCount);
+    let upload = uploadMulter.array('images', imageCount);
 
     return (req, res, next) => {
-    
+
         Promise.coroutine(function* () {
 
             yield new Promise((resolve, reject) => {
